55c0511c6ba3bddd88e393dbf0b3c3cafa0d24e8
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-05-23 09:22:32 +0300

    Add: redirect on 404

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Tweets from "./pages/Tweets/Tweets";
+import NotFoundRedirect from "./components/NotFoundRedirect/NotFoundRedirect";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/tweets" element={<Tweets />} />
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<NotFoundRedirect />} />
     </Routes>
   );
 };
